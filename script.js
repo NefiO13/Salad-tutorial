645d265b72262ad9c1f082a65607fc770fc550b3
NefiO13 <526Nokojie@frhsd.com>
2024-11-20 10:02:06 -0500

    Things are looking good so far

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
-let userName = prompt('What is your name?')
-let userAge = prompt('How old are you?')
-console.log(userName)
-console.log(userAge)
+let Noun = prompt('Please enter a noun')
+let Adjective = prompt('Please enter an adjective')
+let Verb = prompt('Please enter a verb')
+let Time = prompt('Please enter a time')
+let Number = prompt('Please enter any number of your choice')
 
-let myStory = `Hello ${userName}, How as your doing today? How does it feel to be ${userAge} years old?`
-console.log(myStory)
+
+console.log(Noun)
+console.log(Adjective)
+console.log(Verb)
+console.log(Time)
+console.log(Number)
+
+
+const noun = document.getElementById("noun").value
+const verb = document.getElementById("verb").value
+const adjective = document.getElementById("adjective").value
+const place = document.getElementById("place").value
 
 document.getElementById('story').innerHTML = myStory
