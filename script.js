9d05995c59ccfc4c6d9657eea28b081b0875c92e
NefiO13 <526Nokojie@frhsd.com>
2024-11-20 09:38:20 -0500

    ?

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-prompt('What is your name?')
+let userName = prompt('What is your name?')
+let userAge = prompt('How old are you?')
+console.log(userName)
+console.log(userAge)
+
+let myStory = `Hello ${userName}, How as your doing today? How does it feel to be ${userAge} years old?`
+console.log(myStory)
+
+document.getElementById('story').innerHTML = myStory
